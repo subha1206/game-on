136f1a6a2a8ed7bdfb33af0456d94dc239d6660b
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-01-17 12:21:24 +0530

    Updates

---
+++ precache-manifest.266d3f7819c72527e0dc6cc55dabcc1d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f20f4c235cd285ebc2355d111b2f54",
+    "url": "/subha1206/game-on/index.html"
+  },
+  {
+    "revision": "356c6720efea87a1f428",
+    "url": "/subha1206/game-on/static/css/main.ecad7a59.chunk.css"
+  },
+  {
+    "revision": "3282dce3c58275f176f7",
+    "url": "/subha1206/game-on/static/js/2.1a81380c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/subha1206/game-on/static/js/2.1a81380c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "356c6720efea87a1f428",
+    "url": "/subha1206/game-on/static/js/main.6c8aad31.chunk.js"
+  },
+  {
+    "revision": "2272c11cbcebaaa64d67",
+    "url": "/subha1206/game-on/static/js/runtime-main.1af91693.js"
+  },
+  {
+    "revision": "df5cd2ebb767cd77996dbfdc394ab8d4",
+    "url": "/subha1206/game-on/static/media/Notepad.df5cd2eb.jpg"
+  },
+  {
+    "revision": "db9c8e709bfdbbf071cb31a3afd40d98",
+    "url": "/subha1206/game-on/static/media/bell-regular.db9c8e70.svg"
+  },
+  {
+    "revision": "22ccf9526a6d64adb6d0ea07d7d0225b",
+    "url": "/subha1206/game-on/static/media/bell.22ccf952.jpg"
+  },
+  {
+    "revision": "5aab806c794b9cf391845805e1980757",
+    "url": "/subha1206/game-on/static/media/calendar.5aab806c.jpg"
+  },
+  {
+    "revision": "101757d36fd8d70585f0ef7cf6339c6a",
+    "url": "/subha1206/game-on/static/media/clipboard-regular.101757d3.svg"
+  },
+  {
+    "revision": "75e821a9aa9fce8042cf144a43cfed62",
+    "url": "/subha1206/game-on/static/media/envelop.75e821a9.jpg"
+  },
+  {
+    "revision": "a4871e805e7c95f83cb3cc84b776bf1a",
+    "url": "/subha1206/game-on/static/media/gamepad-solid.a4871e80.svg"
+  },
+  {
+    "revision": "3887fd068406c51f246a643d2d84003a",
+    "url": "/subha1206/game-on/static/media/handshake-regular.3887fd06.svg"
+  },
+  {
+    "revision": "92122d7b296c2f72839f14bb979cdb5f",
+    "url": "/subha1206/game-on/static/media/handshake.92122d7b.jpg"
+  },
+  {
+    "revision": "11e425a3c98dd13506e66e22097e2714",
+    "url": "/subha1206/game-on/static/media/spinner-solid.11e425a3.svg"
+  },
+  {
+    "revision": "29def3e5dd923346cc9efa53b9bc0b65",
+    "url": "/subha1206/game-on/static/media/user-regular.29def3e5.svg"
+  }
+]);
